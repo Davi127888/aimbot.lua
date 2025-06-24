678250602fa4e4268fdfb5b3f320d52ffc499907
Davi127888 <davisamuel1251@gmail.com>
2025-06-24 11:24:59 -0300

    Create aimbot.lua

---
+++ aimbot.lua
@@ -0,0 +1,18 @@
+getgenv().serial = {
+    Settings = {
+        Prediction = 0.0440, 
+        AutoClick = false, -- Dont work atm
+    },
+    TargetLock = {
+        LockedTarget = nil, -- dont change
+        AimPart = "Head", 
+        Enabled = true, 
+    },
+    TargetStrafe = {
+        Enabled = true,
+        UseButton = true,
+        Distance = 5,
+        Height = 5,
+        Speed = 20,
+    },
+}
